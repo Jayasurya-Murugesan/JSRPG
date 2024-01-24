58283803a6563de1c5265179e4d9637119f6e4f9
Jayasurya <inboxofsurya@gmail.com>
2024-01-24 10:25:28 +0530

    AddedPlayer and renamed Solution name

---
+++ Engine/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace Engine
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string CharacterClass { get; set; }
+        public int HitPoins { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+    }
+}
